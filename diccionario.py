3f571d22a204b7b71093ead3f66a570b6f76651e
lvente <luisvgomez12@gmail.com>
2025-02-13 13:58:34 -0500

    Se actualiza el diccioanrio

--- diccionario.py
+++ diccionario.py
@@ -1,3 +1,3 @@
-diccionario = {"Luis": 30, "Jenny":33, "Leonor": 53, "Bessie":22, "Kenny":16}
+diccionario = {"Luis": 30, "Jenny":33, "Leonor": 53, "Bessie":22, "Kenny":16, "Milton" : 40}
 
 print(diccionario)
