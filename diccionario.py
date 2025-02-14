51624a9392a394962ab878574401f07ed4bf3d80
lvente <130797968+lvente@users.noreply.github.com>
2025-02-14 11:50:18 -0500

    Se agrega una nueva clave valor

--- diccionario.py
+++ diccionario.py
@@ -1,3 +1,3 @@
-diccionario = {"Luis Vente": 30, "Jenny Vente":33, "Leonor Gomez": 53, "Betsy Vente":22, "Kenny Valencia":16, "Milton Gomez" : 40, "Martin Gomez" : 55, "Pedro Largacha": 70, "Irni Gomez": 49, "Laurencio Gomez" : 52}
+diccionario = {"Luis Vente": 30, "Jenny Vente":33, "Leonor Gomez": 53, "Betsy Vente":22, "Kenny Valencia":16, "Milton Gomez" : 40, "Martin Gomez" : 55, "Pedro Largacha": 70, "Irni Gomez": 49, "Laurencio Gomez" : 52, "Dario Gomez": 31}
 
 print(diccionario)
