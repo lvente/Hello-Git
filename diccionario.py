7162528061f9203e415701a241e4c767270e4fad
lvente <130797968+lvente@users.noreply.github.com>
2025-02-14 11:37:22 -0500

    Update diccionario.py

--- diccionario.py
+++ diccionario.py
@@ -1,3 +1,3 @@
-diccionario = {"Luis": 30, "Jenny":33, "Leonor": 53, "Bessie":22, "Kenny":16, "Milton" : 40, "Martin" : 55, "Pedro": 70, "Irni": 49, "Moron" : 52}
+diccionario = {"Luis Vente": 30, "Jenny":33, "Leonor": 53, "Bessie":22, "Kenny":16, "Milton" : 40, "Martin" : 55, "Pedro": 70, "Irni": 49, "Moron" : 52}
 
 print(diccionario)
